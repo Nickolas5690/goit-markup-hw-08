6b57ac5b5fa231b03f4db810cc6eacded8e50102
Nickolas5690 <ruckopop@gmail.com>
2022-11-16 08:52:16 +0200

    21

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,14 +1,32 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.menu.classList.toggle("is-open");
+//   }
+// })();
+
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-  };
+  
+    const menuBtnRef = document.querySelector("[data-menu-button]");    
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  menuBtnRef.addEventListener("click",() => {
+    const expanded = 
+      menuBtnRef.getAttribute("aria-epanded") === "true" || false;
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-  }
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-epanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-hidden");
+  });
 })();
